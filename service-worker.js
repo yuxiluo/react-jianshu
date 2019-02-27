555e7f0f246d974f94a5e7c1d32400d3f394dfbe
yuxi0530@aliyun.com <yuxi0530@aliyun.com>
2019-02-27 17:16:36 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d8d2bbf581c341d075684e3c4b621576.js"
+  "./precache-manifest.b5de652ab5f50e876b5736833aa77e09.js"
 );
 
 workbox.clientsClaim();
