5959907fbf3ba981cd928dc244b3c8ce925f68fd
yuxi0530@aliyun.com <yuxi0530@aliyun.com>
2019-02-28 09:19:05 +0800

    Updates

---
+++ precache-manifest.6c4f16fdf3efaa73418380aff07ca75a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/react-jianshu/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "f313e5d4a2d3ca752795",
+    "url": "/react-jianshu/static/css/main.2a28d392.chunk.css"
+  },
+  {
+    "revision": "fec8070b173f8fa893f0",
+    "url": "/react-jianshu/static/js/runtime~main.fec8070b.js"
+  },
+  {
+    "revision": "891efc9c938dfc0cedb8ac91ead1476b",
+    "url": "/react-jianshu/static/media/background.891efc9c.jpg"
+  },
+  {
+    "revision": "6fdd5777b0fa3c90f347",
+    "url": "/react-jianshu/static/js/2.6fdd5777.chunk.js"
+  },
+  {
+    "revision": "e4d613f9c05bd54e604356f678a00625",
+    "url": "/react-jianshu/static/media/blank.e4d613f9.jpg"
+  },
+  {
+    "revision": "3ae58974550ca52534418dd941834a80",
+    "url": "/react-jianshu/static/media/hpic01.3ae58974.jpeg"
+  },
+  {
+    "revision": "74d281c2ddc1258a4c7b76eb8e1c5574",
+    "url": "/react-jianshu/static/media/hpic02.74d281c2.jpg"
+  },
+  {
+    "revision": "f313e5d4a2d3ca752795",
+    "url": "/react-jianshu/static/js/main.f313e5d4.chunk.js"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/react-jianshu/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/react-jianshu/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/react-jianshu/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "22096669b4c4b91c3b9266894e951aef",
+    "url": "/react-jianshu/static/media/web-note-ad-side-banner.22096669.png"
+  },
+  {
+    "revision": "7ea3119105a38b19e3795c5df44ee078",
+    "url": "/react-jianshu/static/media/image.7ea31191.jpg"
+  },
+  {
+    "revision": "6fdd5777b0fa3c90f347",
+    "url": "/react-jianshu/static/css/2.71822ab5.chunk.css"
+  },
+  {
+    "revision": "38ea740925f62982f8bf3d77a5ce4927",
+    "url": "/react-jianshu/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.130d66a1f730d101ecf8399e00763758.js"
+  "/react-jianshu/precache-manifest.6c4f16fdf3efaa73418380aff07ca75a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/react-jianshu/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
