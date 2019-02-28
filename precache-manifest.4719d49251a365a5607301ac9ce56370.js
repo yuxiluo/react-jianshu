7509aad3e628d823b15eb76ae86c4cc3fdbda85b
yuxi0530@aliyun.com <yuxi0530@aliyun.com>
2019-02-28 09:39:18 +0800

    Updates

---
+++ precache-manifest.4719d49251a365a5607301ac9ce56370.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "./static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "345a0f2d6ba330c31fde",
+    "url": "./static/css/main.2a28d392.chunk.css"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "891efc9c938dfc0cedb8ac91ead1476b",
+    "url": "./static/media/background.891efc9c.jpg"
+  },
+  {
+    "revision": "234541427176f9369bb8",
+    "url": "./static/js/2.23454142.chunk.js"
+  },
+  {
+    "revision": "e4d613f9c05bd54e604356f678a00625",
+    "url": "./static/media/blank.e4d613f9.jpg"
+  },
+  {
+    "revision": "3ae58974550ca52534418dd941834a80",
+    "url": "./static/media/hpic01.3ae58974.jpeg"
+  },
+  {
+    "revision": "74d281c2ddc1258a4c7b76eb8e1c5574",
+    "url": "./static/media/hpic02.74d281c2.jpg"
+  },
+  {
+    "revision": "345a0f2d6ba330c31fde",
+    "url": "./static/js/main.345a0f2d.chunk.js"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "./static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "./static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "./static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "22096669b4c4b91c3b9266894e951aef",
+    "url": "./static/media/web-note-ad-side-banner.22096669.png"
+  },
+  {
+    "revision": "7ea3119105a38b19e3795c5df44ee078",
+    "url": "./static/media/image.7ea31191.jpg"
+  },
+  {
+    "revision": "234541427176f9369bb8",
+    "url": "./static/css/2.385ff449.chunk.css"
+  },
+  {
+    "revision": "6ea28ccc81c6922999badd225e69bbcd",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-jianshu/precache-manifest.6c4f16fdf3efaa73418380aff07ca75a.js"
+  "./precache-manifest.4719d49251a365a5607301ac9ce56370.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-jianshu/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
